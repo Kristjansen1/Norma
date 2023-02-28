2465979802d31b4cf63bb746da9bc81cd10a4cc7
Kristjan Roskar <roskar.kristjan@gmail.com>
2023-02-28 18:39:17 +0100

    n/a

--- app/src/main/java/roskar/kristjan/norma/model/MonthList.kt
+++
@@ -1,7 +0,0 @@
-package roskar.kristjan.norma.model
-
-class MonthList(val month: String) {
-    override fun toString(): String {
-        return "MonthList(month='$month')"
-    }
-}
--- app/src/main/java/roskar/kristjan/norma/model/NormaList.kt
+++
@@ -1,9 +0,0 @@
-package roskar.kristjan.norma.model
-
-data class NormaList(
-    var date: String,
-    var normaHours: Double,
-    var workingHours: Double,
-    var workplace: String
-    //lol
-)
